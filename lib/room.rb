2d884bcb7ad2b1f1b00b0828f2bdb22f4aa09ff3
vjp888 <rellik888@gmail.com>
2018-10-17 09:18:59 -0600

    Added area method and test

--- lib/room.rb
+++ lib/room.rb
@@ -1,10 +1,13 @@
 class Room
-
-
+attr_reader :category
   def initialize(category, size_x, size_y)
     @category = category
     @size_x = size_x
     @size_y = size_y
+  end
+
+  def area
+    @size_x * @size_y
   end
 
 
--- test/room_test.rb
+++ test/room_test.rb
@@ -9,7 +9,20 @@ class RoomTest <MiniTest::Test
     assert_instance_of Room, room
   end
 
+  def test_room_category
+    room = Room.new(:room, 10, 13)
 
+    assert_equal :room, room.category
+  end
+
+  def test_room_can_calc_area
+
+    room = Room.new(:room, 10, 13)
+
+    assert_equal 130, room.area
+  end
+
+  
 
 
 end
