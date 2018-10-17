891239f667ba84e5c0c778cf0d731270dc9aaaca
vjp888 <rellik888@gmail.com>
2018-10-17 10:06:22 -0600

    Added method and test for total area works, continuing on room call

--- lib/house.rb
+++ lib/house.rb
@@ -11,6 +11,21 @@ attr_reader :address, :rooms, :price
     @rooms << room
   end
 
+  def rooms_from_category(symbol)
+    @rooms.map do |sym|
+      sym.category.match(symbol)
+    end
+
+  end
+
+  def area
+    total = 0
+    @rooms.each do |room|
+      add = room.area
+      total += add
+    end
+    p total
+  end
 
 
 
